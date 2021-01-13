81b5de0680858c67724c1086277f779e978a7e95
Eugen Zdrincu <eugenzdrincu7@gmail.com>
2021-01-13 11:14:00 +0000

    added sentry tracking

--- django_project/settings_prod.py
+++ django_project/settings_prod.py
@@ -1,3 +1,13 @@
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://266e48c56b1a408f8e2da432b3e56262@o504307.ingest.sentry.io/5590940",
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+    send_default_pii=True
+)
+
 SECRET_KEY = config['SECRET_KEY']
 
 with open('/etc/config.json') as config_file:
